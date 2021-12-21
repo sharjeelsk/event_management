19c04b0539868d19519492fbb3c10debc6b884c3
fida-ur-rehman <55063076+fida-ur-rehman@users.noreply.github.com>
2021-12-21 14:54:19 +0530

    ConversationCount and ReminderCount

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,5 +1,7 @@
 const userModel = require("../models/user");
+const Conv = require("../models/conversation");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const { AvailableAddOnPage } = require("twilio/lib/rest/preview/marketplace/availableAddOn");
 
 class User {
@@ -45,55 +47,37 @@ class User {
                         }
                 }
               ],
-              "conversation": [
-                {$match: {_id: req.user._id}},
-                {$lookup: 
-                  {
-                      from: 'conversations',
-                      localField: '_id',
-                      foreignField: 'members',
-                      as: 'conversation'
-                  }},
-                // {$lookup: 
-                // {
-                //     from: 'messages',
-                //     localField: '_id',
-                //     foreignField: 'conversationId',
-                //     as: 'messages'
-                // }},
-                // {
-                //   $addFields: {
-                //     unseen: {
-                //       $sum: { // map out array of seenBy id's //'$$message.seenBy'
-                //         $map: {
-                //           input: "$messages",
-                //           as: "message",
-                //           in:  {
-                //             $cond: {
-                //               if: {
-                //                 $in: [ req.user._id, "$$message.seenBy" ]
-                //               //  $ne: [req.user._id, '$$message.seenBy']
-                //               },
-                //               then: 0,
-                //               else: -1
-                //             }
-                //          }
-                //         }
-                //       }
-                //     }
-                //   }
-                // },
-              ]
             }
           }
         ])
         
-          // aggregate user
-          // get unseen convsersations
-          // get reminders count
         if (user && b) {
-          user.reminderCount = b.reminders
-          return res.status(200).json({ result: user, msg: "Success"});
+          console.log(b[0].user[0])
+          b[0].user[0]['reminderCount'] = b[0].reminders[0].count
+          let conversations = await Conv.aggregate([
+            { $match: { members: {$in: [new mongoose.Types.ObjectId(req.user._id)]}}},
+            { $lookup: {
+               from: 'messages',
+               localField: '_id',
+               foreignField: 'conversationId',
+               as: 'messages'
+           }},
+           { $addFields: {unseen: { $sum: { // map out array of seenBy id's //'$$message.seenBy'
+                   $map: {
+                     input: "$messages",
+                     as: "message",
+                     in:  { $cond: {if: {$in: [ new mongoose.Types.ObjectId(req.user._id), "$$message.seenBy" ] }, then: 0, else: -1}}}}}}
+           }, 
+          //  { $addFields: { lastMsg: {"$arrayElemAt": ["$messages", -1]}}},
+           { $addFields: { unseenMsg: {"$slice": ["$messages", "$unseen"]}}},
+           { $project: { "messages": 0, "unseenMsg": {"conversationId": 0, "createdAt": 0, "seenBy": 0, "sender": 0, "text": 0, "updatedAt": 0, "__v": 0}}}
+          ])
+
+          let conversationCount = conversations.filter(function (el) {
+            return el.unseen < 0
+          });
+          b[0].user[0]['conversationCount'] = conversationCount.length
+          return res.status(200).json({ result: b[0].user[0], msg: "Success"});
         }
       } catch (err) {
         console.log(err)
