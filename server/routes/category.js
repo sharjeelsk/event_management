04f477982b85d02bacb5407ebd65be90f278ee8f
sharjeelsk <54668506+sharjeelsk@users.noreply.github.com>
2022-01-08 11:59:17 +0530

    i c

--- server/routes/category.js
+++ server/routes/category.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const categoryController = require("../controller/category");
 const { isAuthorized } = require("../middleware/reqAuth");
 
-router.get("/all-category", isAuthorized, categoryController.getAllCategory);
+router.get("/all-category", categoryController.getAllCategory);
 router.get("/single-category", isAuthorized, categoryController.getSingleCategory);
 
 router.post("/create-category", categoryController.createCategory);
